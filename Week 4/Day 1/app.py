c940b60cac51d2d9fe83191d6cbc8dd7f74415df
Peter <pi.dudka@gmail.com>
2024-06-25 20:17:58 -0400

    reverting my app.py file from HF

--- Week 4/Day 1/app.py
+++ Week 4/Day 1/app.py
@@ -3,7 +3,9 @@ import chainlit as cl
 from dotenv import load_dotenv
 from operator import itemgetter
 from langchain_huggingface import HuggingFaceEndpoint
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import TextLoader
+from langchain import text_splitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
@@ -16,7 +18,6 @@ from langchain.schema.runnable.config import RunnableConfig
 # ---- ENV VARIABLES ---- # 
 """
 This function will load our environment file (.env) if it is present.
-
 NOTE: Make sure that .env is in your .gitignore file - it is by default, but please ensure it remains there.
 """
 load_dotenv()
@@ -29,6 +30,10 @@ HF_EMBED_ENDPOINT = os.environ["HF_EMBED_ENDPOINT"]
 HF_TOKEN = os.environ["HF_TOKEN"]
 
 # ---- GLOBAL DECLARATIONS ---- #
+# added for Docker purposes compared to run chainlit app
+DATA_DIR = "./data"
+VECTORSTORE_DIR = os.path.join(DATA_DIR, "vectorstore")
+VECTORSTORE_PATH = os.path.join(VECTORSTORE_DIR, "index.faiss")
 
 # -- RETRIEVAL -- #
 """
@@ -39,19 +44,25 @@ HF_TOKEN = os.environ["HF_TOKEN"]
 """
 ### 1. CREATE TEXT LOADER AND LOAD DOCUMENTS
 ### NOTE: PAY ATTENTION TO THE PATH THEY ARE IN. 
-text_loader = 
-documents = 
+document_loader = TextLoader("./data/paul_graham_essays.txt")
+documents = document_loader.load()
 
 ### 2. CREATE TEXT SPLITTER AND SPLIT DOCUMENTS
-text_splitter = 
-split_documents = 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=30)
+split_documents = text_splitter.split_documents(documents)
 
 ### 3. LOAD HUGGINGFACE EMBEDDINGS
-hf_embeddings = 
+hf_embeddings = HuggingFaceEndpointEmbeddings(
+    model=HF_EMBED_ENDPOINT,
+    task="feature-extraction",
+    huggingfacehub_api_token=HF_TOKEN,
+)
 
-if os.path.exists("./data/vectorstore"):
+### 4. INDEX FILES
+### NOTE: REMEMBER TO BATCH THE DOCUMENTS WITH MAXIMUM BATCH SIZE = 32
+if os.path.exists(VECTORSTORE_PATH):
     vectorstore = FAISS.load_local(
-        "./data/vectorstore", 
+        VECTORSTORE_DIR,#"./data/vectorstore", 
         hf_embeddings, 
         allow_dangerous_deserialization=True # this is necessary to load the vectorstore from disk as it's stored as a `.pkl` file.
     )
@@ -59,9 +70,13 @@ if os.path.exists("./data/vectorstore"):
     print("Loaded Vectorstore")
 else:
     print("Indexing Files")
-    os.makedirs("./data/vectorstore", exist_ok=True)
-    ### 4. INDEX FILES
-    ### NOTE: REMEMBER TO BATCH THE DOCUMENTS WITH MAXIMUM BATCH SIZE = 32
+    os.makedirs(VECTORSTORE_DIR, exist_ok=True)
+    for i in range(0, len(split_documents), 32):
+        if i == 0:
+            vectorstore = FAISS.from_documents(split_documents[i:i+32], hf_embeddings)
+            continue
+        vectorstore.add_documents(split_documents[i:i+32])
+    vectorstore.save_local(VECTORSTORE_DIR)
 
 hf_retriever = vectorstore.as_retriever()
 
@@ -71,27 +86,43 @@ hf_retriever = vectorstore.as_retriever()
 2. Create a Prompt Template from the String Template
 """
 ### 1. DEFINE STRING TEMPLATE
-RAG_PROMPT_TEMPLATE = 
+RAG_PROMPT_TEMPLATE = """\
+<|start_header_id|>system<|end_header_id|>
+You are a helpful assistant. You can answer user questions based ONLY on provided context. If you can't answer the question with the provided context, say you don't know.<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+User Query:
+{query}
+Context:
+{context}<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+"""
 
 ### 2. CREATE PROMPT TEMPLATE
-rag_prompt =
+rag_prompt = PromptTemplate.from_template(RAG_PROMPT_TEMPLATE)
 
 # -- GENERATION -- #
 """
 1. Create a HuggingFaceEndpoint for the LLM
 """
 ### 1. CREATE HUGGINGFACE ENDPOINT FOR LLM
-hf_llm = 
+hf_llm = HuggingFaceEndpoint(
+    endpoint_url=HF_LLM_ENDPOINT,
+    max_new_tokens=512,
+    top_k=10,
+    top_p=0.95,
+    temperature=0.3,
+    repetition_penalty=1.15,
+    huggingfacehub_api_token=HF_TOKEN,
+)
 
 @cl.author_rename
 def rename(original_author: str):
     """
     This function can be used to rename the 'author' of a message. 
-
     In this case, we're overriding the 'Assistant' author to be 'Paul Graham Essay Bot'.
     """
     rename_dict = {
-        "Assistant" : "Paul Graham Essay Bot"
+        "Assistant" : "Paul Graham's Personal Historian"
     }
     return rename_dict.get(original_author, original_author)
 
@@ -99,14 +130,15 @@ def rename(original_author: str):
 async def start_chat():
     """
     This function will be called at the start of every user session. 
-
     We will build our LCEL RAG chain here, and store it in the user session. 
-
     The user session is a dictionary that is unique to each user session, and is stored in the memory of the server.
     """
 
     ### BUILD LCEL RAG CHAIN THAT ONLY RETURNS TEXT
-    lcel_rag_chain = 
+    lcel_rag_chain = (
+        {"context": itemgetter("query") | hf_retriever, "query": itemgetter("query")}
+        | rag_prompt | hf_llm
+    )
 
     cl.user_session.set("lcel_rag_chain", lcel_rag_chain)
 
@@ -114,9 +146,7 @@ async def start_chat():
 async def main(message: cl.Message):
     """
     This function will be called every time a message is recieved from a session.
-
     We will use the LCEL RAG chain to generate a response to the user query.
-
     The LCEL RAG chain is stored in the user session, and is unique to each user session - this is why we can access it here.
     """
     lcel_rag_chain = cl.user_session.get("lcel_rag_chain")
